8fde4101019611e37278e8d03fa74c4b9384cc5c
Evan C. Wright <evancwright@yahoo.com>
2017-06-07 21:43:39 -0500

    convert input to ASCII

--- XTAC/bin/Debug/spectrum/io.asm
+++ XTAC/bin/Debug/spectrum/io.asm
@@ -21,18 +21,23 @@ $lp2?	call readkb
 		call zx_to_ascii
 		
 		;echo the char
+		cp 13
+		jp z,$ne?
+		push af	;rst overwrites char
 		rst 16
-		
+		pop af
+$ne?		
 		;store the char in the input buffer
 		push af
-		ld hl,INBUF
+		ld hl,INBUF  ;add buffIx to start of buffer
 		ld d,0
 		ld a,(bufIx)
 		ld e,a
 		add hl,de
 		pop af
-
-		ld (hl),a
+		
+		ld (hl),a ; now store the char
+		
 		;increment the buffer index
 		ld a,(bufIx) 
 		inc a
@@ -40,8 +45,9 @@ $lp2?	call readkb
 				
 		jp $lp2?
 		
-$out?	ld a,13
-		rst 16
+$out?	;ld a,13
+		;rst 16
+		call newline
 		ret
 
 
@@ -56,8 +62,9 @@ OUTLINCR
 		push ix
 		push iy
 		call OUTLIN ; print (hl)
-		ld	a,13  ; new line char
- 		rst 16  ; print char
+;		ld	a,13  ; new line char
+; 		rst 16  ; print char
+		call newline
 		pop iy
 		pop ix
 		pop hl
@@ -95,12 +102,17 @@ $lp?	ld (hl),b
 ;an uppercase ascii char.		
 *MOD
 zx_to_ascii
-		cp 97
-		jp m,$x?	
-		cp 122
-		jp p,$x?
+		cp 97  ; bail if < lowercase a
+		jp c,$x?	
+		cp 122 ; if greater than lowercase z, detokenize?
+		jp nc,$sc?
 		sub 32		;make it uppercase	
-$x?		ret
+		jp $x?
+
+$sc?		
+		sub 165 ; convert to ascii
+$x?		
+		ret
 		
 bufIx DB 0
 INBUF DS 256		
