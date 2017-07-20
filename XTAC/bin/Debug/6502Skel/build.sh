9de6faa1b6ffcf539063753d498c13ee61f6998a
Evan C. Wright <evancwright@yahoo.com>
2017-07-20 00:03:29 -0500

    Apple 2 build changes

--- XTAC/bin/Debug/6502Skel/build.sh
+++ XTAC/bin/Debug/6502Skel/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+B
+echo "This script requires java and AppleCommander-1.3.5.13-ac"
 rm -f errs.txt
 rm -f main.bin
 
@@ -15,10 +16,13 @@ fi
 
 rm -f test.dsk
 echo "creating a disk image"
-java -jar AppleCommander-1.3.5.13-ac.jar -dos140 advent.dsk txtadv
+#java -jar AppleCommander-1.3.5.13-ac.jar -pro140 advent.dsk txtadv
+cp PRODOS.dsk advent.dsk
 
 echo "attaching file to disk image"
 
 java -jar AppleCommander-1.3.5.13-ac.jar -p advent.dsk game.bin bin 0x800 < main.bin
+
+cp advent.dsk "/cygdrive/c/Users/Evan/Documents/Apple2/ADTPro-2.0.2/disks"
 
 ./Applewin.exe -d1 ADOS3.3.dsk -d2 advent.dsk
