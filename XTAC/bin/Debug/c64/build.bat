7ef3689d5ebe37e3426352e82a50c861dad21f53
Evan C. Wright <evancwright@yahoo.com>
2017-08-05 15:05:21 -0500

    works now

--- XTAC/bin/Debug/c64/build.bat
+++ XTAC/bin/Debug/c64/build.bat
@@ -1,19 +1,18 @@
-
-echo "building C64 version"
+echo off
+echo Building for C64...
 
 if EXIST advent.d64 (
    del advent.64
 )
 
-echo "assembling..."
+echo Assembling...
 .\tasm -65 -b -c -s main.asm advent.prg
 
 copy blank.d64 advent.d64
  
-echo "attaching program to disk image..."
-.\c1541 -attach advent.d64 -write advent.prg advent
+echo Attaching program to disk image...
+..\bin\c1541 -attach advent.d64 -write advent.prg advent
 
-echo "Program is ready to run"
-echo "Reminder:"
-echo "LOAD \"$\",8 loads the directory" 
-echo "THE * key is the  ] key"
+echo Program is ready to run
+echo Reminder:
+echo LOAD "$",8 loads the directory
