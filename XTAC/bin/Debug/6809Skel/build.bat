b18030d1ec40178330948847aabf423b18dcfb2d
Evan C. Wright <evancwright@yahoo.com>
2017-08-05 12:45:32 -0500

    works now

--- XTAC/bin/Debug/6809Skel/build.bat
+++ XTAC/bin/Debug/6809Skel/build.bat
@@ -1,30 +1,27 @@
-del /q errs.txt
-del /q game.bin
-
-echo Assembling...
+ECHO OFF 
+DEL /q errs.txt
+DEL /q game.bin
+ 
+ECHO Assembling...
 ..\bin\lwasm main.asm --6809 --list=game.list --output=game.bin 2> errs.txt
 
-echo "files assembled"
-
-FOR %%A IN ("errs.txt") DO SET size=%%~zA
-
-if %size% gtr 0
-(
-   echo "Errors occured."
-   echo "Halting."
-)
-else
-(
-   echo "Attaching file to disk image"
-   .\writecocofile advent.dsk game.bin 2>> errs.txt
+ 
+FOR %%A IN ("errs.txt") DO SET /A size = %%~zA
+ 
+IF %size% GTR 0 (
+   ECHO Errors occured.
+   ECHO See errs.txt for details.
+   EXIT /B 1
+)ELSE (
+   ECHO Attaching file to disk image
+   ..\bin\writecocofile advent.dsk game.bin 2> errs.txt
    
-   for %%A in ("errs.txt") do set size=%%~zA
-   if %size% gtr 0
-   (
-        echo "Unable to attach .bin file to disk image.  Is it open in an emulator?"
-   )
-   else 
-   (
-		echo "done"
+   FOR %%A IN ("errs.txt") DO SET /A size  = %%~zA
+   IF %size% GTR 0 (
+        ECHO Unable to attach .bin file to disk image.  Is it open in an emulator?
+		EXIT /B 1
+   ) ELSE (
+		echo done.
+		echo the file game.bin has been attached to advent.dsk
 	)
 )
