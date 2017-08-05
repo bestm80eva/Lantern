aab02608a0fa326211833abb898b31e7415d4c24
Evan C. Wright <evancwright@yahoo.com>
2017-08-05 14:31:08 -0500

    works now

--- XTAC/bin/Debug/apple2/build.bat
+++ XTAC/bin/Debug/apple2/build.bat
@@ -1,33 +1,32 @@
-echo "This script requires java and AppleCommander-1.3.5.13-ac"
-del errs.txt
-del main.bin
+echo off
+echo Just FYI: This script requires java runtime and AppleCommander-1.3.5.13-ac
+del /q errs.txt
+del /q main.bin
 
 tasm.exe main.asm -b -65 2> errs.txt
 
-ren main.obj main.bin
-
+REN main.obj main.bin
 
 FOR %%A IN ("errs.txt") DO set size=%%~zA
 IF %size%  GTR 0 (
-	echo  "errors occured."
-	exit 1
+	ECHO  Errors occured.
+	ECHO  See errs.txt for details.
+	EXIT /B 1
 )
 
 
-echo "creating a disk image"
-#java -jar AppleCommander-1.3.5.13-ac.jar -pro140 advent.dsk txtadv
-cp PRODOS.dsk advent.dsk
+ECHO Creating a disk image
+:: java -jar AppleCommander-1.3.5.13-ac.jar -pro140 advent.dsk txtadv
+COPY PRODOS.dsk advent.dsk
 
-echo "attaching file to disk image"
+ECHO Attaching file to disk image.
 
 java -jar AppleCommander-1.3.5.13-ac.jar -p advent.dsk game.bin bin 0x800 < main.bin
 
-copy advent.dsk "/cygdrive/c/Users/Evan/Documents/Apple2/ADTPro-2.0.2/disks"
+:: copy advent.dsk "/cygdrive/c/Users/Evan/Documents/Apple2/ADTPro-2.0.2/disks"
 
-.\Applewin.exe -d1 advent.dsk -d2 PRODOS.dsk
+:: .\Applewin.exe -d1 advent.dsk -d2 PRODOS.dsk
 
-:filesize
-	set size=%~z1
-	exit /b 0
-
+echo Done.  File is attached to advent.dsk
+ 
 
