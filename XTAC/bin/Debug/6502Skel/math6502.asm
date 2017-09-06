e5304cc1ceb80eacb48fd905c0c1b808c4c1dd6f
Evan C. Wright <evancwright@yahoo.com>
2017-09-06 18:37:21 -0500

    rand stuff

--- XTAC/bin/Debug/6502Skel/math6502.asm
+++ XTAC/bin/Debug/6502Skel/math6502.asm
@@ -3,7 +3,41 @@
 
 
 next_rand
-		rts
+	pha
+	tax
+	pha
+	tay
+	pha
+	lda lastRand ; get tap 1
+	and $rtap1  
+	sta $rtemp
+	lda lastRand ; get tap 2
+	and $rtap2
+	lsr	;light up bits
+	lsr
+	eor $rtap2 
+	sta $rtemp  ; new bit
+	lda lastRand ; shift seed right
+	lsr
+	sta lastRand ; and store it
+	lda $rtemp  ;get the new bit
+	asl ;put the bit on the left
+	asl
+	asl
+	asl
+	asl
+	asl
+	asl
+	ora $rtemp ; or it
+	sta $temp
+	sta rval
+	dec rval
+	pla
+	tay
+	pla
+	tax
+	pla
+	rts
  
 ;mod a by y	
 .module	div
@@ -22,12 +56,15 @@ _lp	cmp divisor
 _x	sta remainder
 	rts
 
-
+ 
 divisor .byte 0
 remainder .byte 0
 divResult .byte 0
 lastRand .byte	0	
-seed .byte 0
+seed .byte 255
 rmask1 .byte 1
 rmask2 .byte 3
-
+rtap1 .byte 0
+rtap2 .byte 0
+rtemp .byte 0
+rval .byte 0
